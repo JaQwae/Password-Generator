3679a4a76b58581542e8e53e1cb36865d4551bce
JaQwaee <jaqwaeellison@gmail.com>
2022-07-09 16:17:27 -0500

    welcome alert added more conditional statements add for selecting numbers function

--- password-generator/script.js
+++ password-generator/script.js
@@ -8,6 +8,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   
   //inputting the number of character selected
+  welcomeMessage()
   selectingNumberOfCharacters()
 
   var password = generatePassword();
@@ -20,17 +21,30 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//Welcome message
+function welcomeMessage(){
+  let intro = alert('Hello, welcome to password generator. This program will allow you to randomly generate a password within the criteria you select. Press okay to get started')
+}
+
 //User selecting number of characters
 function selectingNumberOfCharacters() {
-  var numbersSelected = prompt('Please enter the number of characters that you would like your password to be. (Min=8 and Max=128)')
+  
+  let numbersSelected = prompt('Please enter the number of characters that you would like your password to be. (Min=8 and Max=128)') 
+
     if (numbersSelected < 8) {
-      prompt('Password length is too short, please enter a number that is at least 8.')
-
-      } else {
-        return numberselected
+      alert('Password length is too short, please enter a number that is at least 8.')
+      selectingNumberOfCharacters()
+    
+    }else if (numbersSelected > 128) {
+      alert('Password length is too long, please enter a number that is less than 129.')
+      selectingNumberOfCharacters()
+    
+      //setting condition that only allow numbers
+    }//else if (numbersSelected == ){
+      //alert('Not a valid input, please enter a number')
+      //selectingNumberOfCharacters()}
+    
+    else{
+        return numberselected;
       }
-
-      // } elif (numbersSelected > 128) {
-      //   prompt('Password length is too long, please enter a number that is at least 8.')
-
 }
