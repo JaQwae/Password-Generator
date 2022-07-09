a923b58042ce086f0a63f2b3361160ab04050407
JaQwaee <jaqwaeellison@gmail.com>
2022-07-09 14:02:39 -0500

    first prompt pop up once button is pressed, linked the selecting number function to wrire password

--- password-generator/script.js
+++ password-generator/script.js
@@ -6,6 +6,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  
+  //inputting the number of character selected
+  selectingNumberOfCharacters()
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -15,3 +19,8 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//User selecting number of characters
+function selectingNumberOfCharacters() {
+  var numbersSelected = prompt('Please select the length of your password. (Min=8 and Max=128)')
+}
