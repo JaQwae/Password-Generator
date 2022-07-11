5de2198648e3b4fcf4bf2f099952ff97438d9341
JaQwaee <jaqwaeellison@gmail.com>
2022-07-11 18:00:19 -0500

    Updated comments on js file

--- password-generator/script.js
+++ password-generator/script.js
@@ -6,10 +6,10 @@ var confirmNumber;
 var confirmCharacter;
 var confirmLowercase;
 var confirmUppercase;
-var upperArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lowerArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var numbersArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var specialArr = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '<', '>', ',', '.'];
+var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '<', '>', ',', '.'];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -43,12 +43,13 @@ function generatePassword() {
   //displays what was selected
   alert('Your password will contain ' + confirmLength + ' characters');
 
+  //allow you to select the type of characters your password will consist of
   var confirmUpperCase = confirm('Click OK for your password to contain UPPERCASE letters.');
   var confirmLowerCase = confirm('Click OK for your password to contain LOWERCASE letters.');
   var confirmNumericCharacter = confirm('Click OK for your password to contain NUMBERS.');
   var confirmSpecialCharacter = confirm('Click OK for your password to contain SPECIAL characters.');
 
-  //if no options were no
+  //if no character type options were selected
   while (confirmUpperCase === false && confirmLowerCase === false && confirmNumericCharacter === false && confirmSpecialCharacter === false) {
       alert('You must choose at lease one parameter');
       var confirmUpperCase = confirm('Click OK for your password to contain UPPERCASE letters.');
@@ -59,29 +60,31 @@ function generatePassword() {
 
   var finalPassword = [];
 
-  //Adds selected character type to an empty array, all possible combinations (*talk with tutor it see why function was making this undefine also why concat*)
+  //Adds selected character type to an empty array, all possible combinations (*note to self talk with tutor/TA to see why function was making this undefine also why concat*)
     if (confirmSpecialCharacter=== true ) {
-      finalPassword = finalPassword.concat(specialArr) }; 
+      finalPassword = finalPassword.concat(special) }; 
 
     if (confirmUpperCase=== true ) {
-      finalPassword = finalPassword.concat(upperArr) }; 
+      finalPassword = finalPassword.concat(upper) }; 
 
     if (confirmLowerCase=== true ) {
-      finalPassword = finalPassword.concat(lowerArr) }; 
+      finalPassword = finalPassword.concat(lower) }; 
     
     if (confirmNumber=== true ) {
-      finalPassword = finalPassword.concat(numberArr) }; 
+      finalPassword = finalPassword.concat(number) }; 
     
   
 
   var randomPassword = ''
-
+  
+  //Generates random combinations of characters
   for (var i = 0; i < confirmLength; i++) {
     randomPassword = randomPassword + finalPassword[Math.floor(Math.random() * finalPassword.length)];
     
   }
-  
+
   return randomPassword;
+  
 }
 
 
